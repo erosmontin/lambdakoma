4df06e2567bfac270853ed9b5ef785624cb7d3d8
eros.montin <eros.montin@gmail.com>
2024-10-01 15:54:38 -0400

    Hackathon24

--- pipeline/script.py
+++ pipeline/script.py
@@ -29,7 +29,7 @@ NT=10
 GPU=False
 A=pn.Pathable(OUTDIR+'/')
 A.ensureDirectoryExistence()
-T,SL=c.process_slice(SL, B0, MODEL, PROP, SEQ, OUTDIR, GPU,NT,SENSITIVTIES=FIELD["b1m"])
+T,SL=c.process_slice(SL, B0, MODEL, PROP, SEQ, OUTDIR, GPU,NT,SENSITIVITIES=FIELD["b1m"])
 import matplotlib.pyplot as plt
 plt.imshow(np.abs(T), cmap='gray')
 _tim=L.stop()
