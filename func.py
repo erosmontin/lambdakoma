0977b55d1d865303be7fa2820cbe437c108048b1
eros.montin <eros.montin@gmail.com>
2024-10-04 14:04:45 -0400

    Hackathon24_day+3

--- func.py
+++ func.py
@@ -1,8 +1,11 @@
 import pyvista as pv
 import meshio
-def read_gmsh_file(file_path):
+import numpy as np
+from scipy.spatial.transform import Rotation as R
+
+def read_gmsh_file(file_path,scale=1000):
     mesh = meshio.read(file_path)
-    mesh.points *= 1000
+    mesh.points *= scale
     unstructured_grid = pv.wrap(mesh)
     return unstructured_grid
 
@@ -11,3 +14,71 @@ def generate_points_on_side(origin, direction, length, n):
     step_size = length / (n - 1)  # Calculate the step size
     points = [origin + direction * step_size * i for i in range(n)]
     return points
+def add_rectangle_to_plot(plotter, length, width, height, center,rotation=[0,90,45]):
+    # Create a box (rectangular prism)
+    box = pv.Box(bounds=(0, length, 0, width, 0, height))
+    
+    box.points += np.array(center)
+    plotter.add_mesh(box, color='yellow', opacity=0)
+
+    box.rotate_x(rotation[0],inplace=True)
+    box.rotate_y(rotation[1],inplace=True)
+    box.rotate_z(rotation[2],inplace=True)
+       # Define the box's local x, y, and z axes
+    local_axes = np.array([[1, 0, 0], [0, 1, 0], [0, 0, 1]])
+
+    # Calculate the rotation matrix
+    r = R.from_euler('xyz', rotation, degrees=True)
+    rotation_matrix = r.as_matrix()
+
+    # Rotate the local axes with the box
+    rotated_axes = rotation_matrix @ local_axes.T
+
+    # Normalize the rotated axes to unit vectors
+    normalized_axes = rotated_axes / np.linalg.norm(rotated_axes, axis=0)
+
+    # Define the global x, y, and z axes
+    global_axes = np.eye(3)
+
+    # Calculate the direction cosines
+    direction_cosines = normalized_axes.T @ global_axes
+
+    # Calculate the origin after rotation
+    origin = rotation_matrix @ np.array(center)
+
+    return direction_cosines,origin,
+
+def add_slice(plotter, KSPACESIZE, ORIGINKSPACE,D,rotation=[0,0,0],n=64):
+    S=KSPACESIZE
+    S[2]=S[2]//2+ORIGINKSPACE[2]
+    directors,origin=add_rectangle_to_plot(plotter, *S, center=ORIGINKSPACE,rotation=rotation)
+    first_direction = directors[0]
+    points = generate_points_on_side(origin, first_direction, KSPACESIZE[0], n)
+
+    INDEX=[]
+    for point in points:
+        plotter.add_points(point, color='red')
+        INDEX.append(D.getIndexFromCoordinates(point))
+
+
+    second_direction = directors[1]
+    # import time
+    for point in points:
+        # time.sleep(0.1)
+        line = pv.Line(point, point + second_direction * KSPACESIZE[1])
+        plotter.add_mesh(line, color='blue')
+
+
+    res=KSPACESIZE
+    res[0]=float(res[0])/n
+    res[2]=float(res[2])/1
+
+    OUT={
+        "points":[list(t) for t in points],
+        "index":INDEX,
+        "direction":directors,
+        "origin":list(origin),
+        "spacing":res,
+        "KSIZE":KSPACESIZE
+    }
+    return OUT
--- viewer.py
+++ viewer.py
@@ -36,7 +36,7 @@ isosurface.transform(header.get_base_affine(),inplace=True)
 isosurface.rotate_z(180)
 
 #smooth isosurface
-isosurface = isosurface.smooth(n_iter=100, relaxation_factor=0.1)
+isosurface = isosurface.smooth(n_iter=10, relaxation_factor=0.1)
 
 plotter.add_axes()
 
@@ -79,108 +79,32 @@ isosurface.rotate_z(180)
 plotter.add_mesh(isosurface, 'blue',opacity=0.8)
 
 
-# def callback(*args):
-#     # Get the click position
-#     x, y = args[0], args[1]
-#     position = plotter.pick_mouse_position((x, y))
-
-#     # Add a sphere at the click position
-#     if position is not None:
-#         sphere = pv.Sphere(radius=0.1, center=position)
-#         plotter.add_mesh(sphere)
-from scipy.spatial.transform import Rotation as R
-
-def add_rectangle_to_plot(plotter, length, width, height, center,rotation=[0,0,0]):
-    # Create a box (rectangular prism)
-    box = pv.Box(bounds=(0, length, 0, width, 0, height))
-    
-    box.points += np.array(center)
-    plotter.add_mesh(box, color='yellow', opacity=0.2)
-
-    box.rotate_x(rotation[0],inplace=True)
-    box.rotate_y(rotation[1],inplace=True)
-    box.rotate_z(rotation[2],inplace=True)
-       # Define the box's local x, y, and z axes
-    local_axes = np.array([[1, 0, 0], [0, 1, 0], [0, 0, 1]])
-
-    # Calculate the rotation matrix
-    r = R.from_euler('xyz', rotation, degrees=True)
-    rotation_matrix = r.as_matrix()
-
-    # Rotate the local axes with the box
-    rotated_axes = rotation_matrix @ local_axes.T
-
-    # Normalize the rotated axes to unit vectors
-    normalized_axes = rotated_axes / np.linalg.norm(rotated_axes, axis=0)
-
-    # Define the global x, y, and z axes
-    global_axes = np.eye(3)
-
-    # Calculate the direction cosines
-    direction_cosines = normalized_axes.T @ global_axes
-
-    # Calculate the origin after rotation
-    origin = rotation_matrix @ np.array(center)
-
-    return direction_cosines,origin,
-
-
-KSPACESIZE=[256,256,6]
-ORIGINKSPACE=[-KSPACESIZE[0]//2,-KSPACESIZE[1]//2,20]
-directors,origin=add_rectangle_to_plot(plotter, *KSPACESIZE, center=ORIGINKSPACE,rotation=[0,0,0])
-
-print(directors)
-n = 64
-first_direction = directors[0]
-points = generate_points_on_side(origin, first_direction, KSPACESIZE[0], n)
-
-
-# Add the points to the plot
-import pyable_eros_montin.imaginable as ima
-D=ima.Imaginable(MODEL)
-INDEX=[]
-for point in points:
-    plotter.add_points(point, color='red')
-    INDEX.append(D.getIndexFromCoordinates(point))
-
-
-second_direction = directors[1]
-# import time
-for point in points:
-    # time.sleep(0.1)
-    line = pv.Line(point, point + second_direction * KSPACESIZE[1])
-    plotter.add_mesh(line, color='blue')
-
-
-
 
 
 unstructured_grid = read_gmsh_file('/g/models/data/coil/GMT_decoupled_shield_rad_10.246cm_wide_3.9cm_len_22cm_width_1cm_mesh_0.005.msh')
 
 plotter.add_mesh(unstructured_grid,opacity=0.1)
 
-# Set the callback function
-# plotter.iren.add_observer('LeftButtonPressEvent', callback)
-# Start the plotter's interaction loop
 
 
 
 
-res=KSPACESIZE
-res[0]=float(res[0])/n
-res[2]=float(res[2])/1
+for i in [10,40,80]:
+    KSPACESIZE=[256,256,6]    
+    ORIGINKSPACE=[-KSPACESIZE[0]//2,-KSPACESIZE[1]//2,i]
+    D=ima.Imaginable(MODEL)
+    OUT=[]
+    OUT.append(add_slice(plotter, KSPACESIZE, ORIGINKSPACE,D,rotation=[0,90,0],n=64))
+    for key, value in OUT[-1].items():
+        if isinstance(value, np.ndarray):
+            OUT[-1][key] = value.tolist()
 
-OUT={
-    "points":[list(t) for t in points],
-    "index":INDEX,
-    "direction":directors,
-    "origin":list(origin),
-    "spacing":res,
-    "KSIZE":KSPACESIZE
-}
+
+
+# Convert numpy arrays to lists
 
 import pynico_eros_montin.pynico as pn
-O=pn.Pathable("./OUT.json")
+O=pn.Pathable("OUT.json")
 O.writeJson(OUT)
 
 plotter.show()
