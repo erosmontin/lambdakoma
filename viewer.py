baf089aa270265a3d65d16a2c3cf4ccfba04f537
eros.montin <eros.montin@gmail.com>
2024-10-04 12:23:44 -0400

    Hackathon24_day+3

--- viewer.py
+++ viewer.py
@@ -9,7 +9,8 @@ from PyQt5 import QtWidgets
 from func import *
 import sys
 import pyable_eros_montin.imaginable as ima
-img = nib.load('/g/models/data/sigma_e.nii.gz')
+MODEL='/g/models/data/sigma_e.nii.gz'
+img = nib.load(MODEL)
 
 b1 = nib.load('/g/models/data/b1m_001.nii.gz')
 
@@ -125,24 +126,28 @@ def add_rectangle_to_plot(plotter, length, width, height, center,rotation=[0,0,0
 
 
 KSPACESIZE=[256,256,6]
-ORIGINKSPACE=[-KSPACESIZE[0]//2,-KSPACESIZE[1]//2,0]
+ORIGINKSPACE=[-KSPACESIZE[0]//2,-KSPACESIZE[1]//2,20]
 directors,origin=add_rectangle_to_plot(plotter, *KSPACESIZE, center=ORIGINKSPACE,rotation=[0,0,0])
 
 print(directors)
-n = 128
+n = 64
 first_direction = directors[0]
 points = generate_points_on_side(origin, first_direction, KSPACESIZE[0], n)
 
 
 # Add the points to the plot
+import pyable_eros_montin.imaginable as ima
+D=ima.Imaginable(MODEL)
+INDEX=[]
 for point in points:
     plotter.add_points(point, color='red')
+    INDEX.append(D.getIndexFromCoordinates(point))
 
 
 second_direction = directors[1]
-import time
+# import time
 for point in points:
-    time.sleep(0.1)
+    # time.sleep(0.1)
     line = pv.Line(point, point + second_direction * KSPACESIZE[1])
     plotter.add_mesh(line, color='blue')
 
@@ -157,6 +162,29 @@ plotter.add_mesh(unstructured_grid,opacity=0.1)
 # Set the callback function
 # plotter.iren.add_observer('LeftButtonPressEvent', callback)
 # Start the plotter's interaction loop
+
+
+
+
+res=KSPACESIZE
+res[0]=float(res[0])/n
+res[2]=float(res[2])/1
+
+OUT={
+    "points":[list(t) for t in points],
+    "index":INDEX,
+    "direction":directors,
+    "origin":list(origin),
+    "spacing":res,
+    "KSIZE":KSPACESIZE
+}
+
+import pynico_eros_montin.pynico as pn
+O=pn.Pathable("./OUT.json")
+O.writeJson(OUT)
+
 plotter.show()
+
+
 
 sys.exit(app.exec_())
