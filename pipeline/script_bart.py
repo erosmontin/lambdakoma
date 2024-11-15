688afbfdd7b65ce3976f870a796383b52711a302
Eros <eros.montin@gmail.com>
2024-11-15 10:40:22 -0500

    post-ismrm25-abstract

--- pipeline/script_bart.py
+++ pipeline/script_bart.py
@@ -11,8 +11,12 @@ L.start()
 B=pn.BashIt()
 
 #customize the calculation
+direction="axial"
+if len(sys.argv) > 3:
+    direction = sys.argv[3]
+    
 
-OUTDIR="/g/KOMA_s2"
+OUTDIR=f"/g/B_T1_3T/{direction}"
 if len(sys.argv) > 1:
     OUTDIR = sys.argv[1]
 
@@ -20,14 +24,26 @@ import os
 os.makedirs(OUTDIR,exist_ok=True)
 
 
-SL=90
+SL=85
 if len(sys.argv) > 2:
     SL = int(sys.argv[2])
+    
+#     TE10 TR600 for T1-weighted
+# TE80 TR4000 for T2-weighted
+# TE10_TR4000 for rho-weighted
 
 # SEQ="seq/sdl_pypulseq_TE10_TR4000_os2_largeCrush_xSpoil.seq" #1
-# SEQ="seq/sdl_pypulseq_TE10_TR600_os2_largeCrush_xSpoil.seq" #3
-SEQ="seq/sdl_pypulseq_TE80_TR4000_os2_largeCrush_xSpoil.seq" #2
+SEQ="seq/sdl_pypulseq_TE10_TR600_os2_largeCrush_xSpoil.seq" #3
+# SEQ="seq/sdl_pypulseq_TE80_TR4000_os2_largeCrush_xSpoil.seq" #2
 
+
+coil="cloudMR_birdcagecoil.zip"
+if len(sys.argv) > 4:
+    coil = sys.argv[4]
+
+
+
+print(f"OUTDIR={OUTDIR}",f"SL={SL}",f"SEQ={SEQ}",f"coil={coil}",f"direction={direction}")
 
 T1=None
 T2=None
@@ -41,16 +57,18 @@ import pyable_eros_montin.imaginable as ima
 import numpy as np
 
 
-FIELD=c.readMarieOutput("cloudMR_overlap.zip")
+# FIELD=c.readMarieOutput("cloudMR_overlap.zip")
+# FIELD=c.readMarieOutput("cloudMR_triangularcoil.zip")
+FIELD=c.readMarieOutput(coil)
 
 import SimpleITK as sitk
 B0=FIELD["B0"]
-NT=20
+NT=10
 GPU=False
 A=pn.Pathable(OUTDIR+'/')
 A.ensureDirectoryExistence()
 SENS_DIR=pn.Pathable(FIELD["b1m"][0]).getPath()
-desired_spin_resolution = (2e-3,2e-3)
+desired_spin_resolution = (2e-3,2e-3,2e-3)
 
 NC=sitk.ReadImage(FIELD["NC"])
 
@@ -64,7 +82,7 @@ k=OUTDIR + f"/{SL}/k.npz"
 if os.path.exists(k):
     data=np.load(k)["data"]
 else:
-    data=c.simulate_2D_slice(SL, B0, FIELD["T1"],FIELD["T2"],FIELD["T2star"],FIELD["dW"],FIELD["PD"],desired_spin_resolution,SEQ,OUTDIR,SENS_DIR,GPU,NT,debug=True)
+    data=c.simulate_2D_slice(SL, B0, FIELD["T1"],FIELD["T2"],FIELD["T2star"],FIELD["dW"],FIELD["PD"],desired_spin_resolution,direction,SEQ,OUTDIR,SENS_DIR,GPU,NT,debug=True)
     np.savez(k,data=data)
 
 BARTDIR=pn.createRandomTemporaryPathableFromFileName('a.cfl')
